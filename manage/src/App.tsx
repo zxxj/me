1b18b4d4498d3305eb03ea4c134804f46448ad5c
zhangxinxin <1285367184@qq.com>
2025-06-09 23:40:19 +0800

    feat(初始化项目): 删除初始化默认文件,引入tailwindcss

---
+++ manage/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        app
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ manage/src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+
+export const Login: React.FC = () =>{
+
+	return (
+		<div>
+				<h1>Login</h1>
+		</div>
+	)
+}
