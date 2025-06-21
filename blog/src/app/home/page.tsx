7069e67d37554d72e30cbc146f8c3f5f4afac6c0
zhangxinxin <1285367184@qq.com>
2025-06-21 23:54:15 +0800

    feat(初始动画): 初始动画最后透明度设置为1/loading动画最初透明度设置为0避免刷新时与初始动画重叠

--- blog/src/app/home/page.tsx
+++ blog/src/app/home/page.tsx
@@ -2,7 +2,7 @@ import { getPostList } from '@/app/service/modules/index';
 import PostList from './PostList'; // 引入客户端组件
 
 export default async function HomePage() {
-  await new Promise((res) => setTimeout(res, 5000)); // 2 秒
+  await new Promise((res) => setTimeout(res, 3000));
 
   const data = (await getPostList()).data.data.posts;
   console.log(data);
