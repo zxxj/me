d9004dda9ffa00a3a4944dd746daba9bd0e9c42f
zhangxinxin <1285367184@qq.com>
2025-06-23 01:30:16 +0800

    feat(作品集): 作品集页面搭建

--- blog/src/app/projects/page.tsx
+++ blog/src/app/projects/page.tsx
@@ -1,9 +1,8 @@
 import { getPostList } from '@/app/service/modules/index';
-// import PostList from './PostList'; // 引入客户端组件
+import Wrapper from './Wrapper';
 
 export default async function ProjectsPage() {
   await new Promise((res) => setTimeout(res, 3000));
 
-  // return <PostList data={data} />;
-  
+  return <Wrapper />;
 }
